0dcc5285acdd1709b7654ed3883d779a29eb318f
Abhijit Sarkar <contact@asarkar.com>
2024-12-26 15:44:13 -0800

    Complete some exercises on infinite lists

--- src/InfiniteList.hs
+++ src/InfiniteList.hs
@@ -1,3 +1,6 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
 module InfiniteList where
 
 import Prelude hiding (cycle)
@@ -14,6 +17,8 @@ ys and zs are in strictly increasing order, we also have
 
 Does it follow that in the expression foldr1 xmerge multiples we could replace
 foldr1 by foldl1?
+
+ANSWER: No, since foldl1 f xs = undefined for any infinite list xs.
 -}
 
 {-
@@ -31,3 +36,49 @@ and infinite lists.
 cycle :: [a] -> [a]
 cycle [] = error "empty list"
 cycle xs = ys where ys = xs ++ ys
+
+{-
+Exercise C
+
+The fibonacci function is defined by
+	fib 0 = 0
+	fib 1 = 1
+	fib n = fib (n-1) + fib (n-2)
+
+Write down a one-line definition of the list fibs that produces the infinite list of
+Fibonacci numbers.
+-}
+
+fibs :: [Integer]
+-- fibs = xs where xs = 0 : 1 : [xs !! 0 + xs !! 1]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+{-
+Exercise D
+
+A well-known problem, due to the mathematician W.R. Hamming, is to write a
+program that produces an infinite list of numbers with the following properties:
+(i) the list is in strictly increasing order;
+(ii) the list begins with the number 1;
+(iii) if the list contains the number x, then it also contains the numbers 2x, 3x and 5x;
+(iv) the list contains no other numbers.
+Thus, the required list begins with the numbers
+	1,2,3,4,5,6,8,9,10,12,15,16,...
+
+Write a definition of hamming that produces this list.
+-}
+
+hamming :: [Integer]
+hamming = 1 : mergeAll [map (2 *) hamming, map (3 *) hamming, map (5 *) hamming]
+
+mergeAll :: (Ord a) => [[a]] -> [a]
+mergeAll = foldr1 xmerge
+
+xmerge :: (Ord a) => [a] -> [a] -> [a]
+xmerge (x : xs) ys = x : merge xs ys
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge (x : xs) (y : ys)
+  | x < y = x : merge xs (y : ys)
+  | x == y = x : merge xs ys
+  | x > y = y : merge (x : xs) ys
--- test/InfiniteListSpec.hs
+++ test/InfiniteListSpec.hs
@@ -14,5 +14,14 @@ spec = do
     it "cycles infinite list" $
       take 7 (cycle [1 ..]) `shouldBe` [1 :: Int .. 7]
 
+    -- shouldThrow expects a first argument of type `IO a`,
+    -- whereas `cycle` returns `[a], so, we use evaluate to
+    -- convert it to `IO [a]`.
     it "throws error on empty list" $
       B.evaluate (cycle []) `shouldThrow` anyErrorCall
+
+    it "produces the infinite list of Fibonacci numbers" $
+      and (zipWith (==) fibs [0, 1, 1, 2, 3, 5, 8]) `shouldBe` True
+
+    it "produces the infinite list of Hamming numbers" $
+      and (zipWith (==) hamming [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16]) `shouldBe` True
